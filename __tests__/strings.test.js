b09ee96cace9e52d5634e4c18e76ec296b1dcbf7
Robert Romero <robert.e.romero@gmail.com>
2025-07-17 13:12:14 -0700

    Improve titleCase and add tests

--- __tests__/strings.test.js
+++ __tests__/strings.test.js
@@ -4,6 +4,8 @@ describe('titleCase', () => {
   test('converts words to title case', () => {
     expect(titleCase('hello world')).toBe('Hello World');
     expect(titleCase('MULTIPLE words here')).toBe('Multiple Words Here');
+    expect(titleCase('  many   spaces  ')).toBe('Many Spaces');
+    expect(titleCase('line\nbreaks')).toBe('Line Breaks');
   });
 
   test('handles empty strings', () => {
